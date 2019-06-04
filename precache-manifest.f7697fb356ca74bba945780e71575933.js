6121d618962157115d6dce809a4d5ac7fd20019e
Patrick Glen Kennedy <pkennedytx1@Patricks-MacBook-Pro.local>
2019-06-04 15:38:06 -0500

    Updates

---
+++ precache-manifest.f7697fb356ca74bba945780e71575933.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8dd3e95aea4a51757eee7549dce324f",
+    "url": "/theportfolio/index.html"
+  },
+  {
+    "revision": "5ba5e4a66b481fe48242",
+    "url": "/theportfolio/static/css/2.edad4f92.chunk.css"
+  },
+  {
+    "revision": "9e2a259a4d16d2b9ecae",
+    "url": "/theportfolio/static/css/main.abd4c59e.chunk.css"
+  },
+  {
+    "revision": "5ba5e4a66b481fe48242",
+    "url": "/theportfolio/static/js/2.dde33e4e.chunk.js"
+  },
+  {
+    "revision": "9e2a259a4d16d2b9ecae",
+    "url": "/theportfolio/static/js/main.6d675312.chunk.js"
+  },
+  {
+    "revision": "493b9a9451e4574352a9",
+    "url": "/theportfolio/static/js/runtime~main.c9011835.js"
+  }
+]);
