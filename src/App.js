1a5a1b806e6bbb6c3ba3e914e02a9ec7c692254d
Patrick Glen Kennedy <pkennedytx1@Patricks-MacBook-Pro.local>
2019-06-04 15:07:10 -0500

    deployment issuessues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './component/Navbar';
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Landing from './component/Landing';
 import Wrapper from './component/Wrapper';
 import Aboutme from './component/Aboutme';
@@ -12,7 +12,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/theportfolio">
     <div id="top" className="App">
       <Navbar />
       <Wrapper>
